56aff35eccbf7ab2d77b97199e7b82b7356dfd0b
KonradKisiel <konrad.kisiel.webmaster@gmail.com>
2019-01-08 17:09:07 +0100

    displaying data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import Form from './components/Form';
+import WeatherInfo from './components/WeatherInfo';
 
 //register to OpenWeatherMap and paste API key here
-const API_KEY = "";
+const API_KEY = "37ed176a584be416c6960e386d5d239c";
 let units = "metric"; //imperial also available
 
 class App extends Component {
@@ -22,7 +23,8 @@ class App extends Component {
         temperature: data.main.temp,
         description: data.weather[0].description,
         pressure: data.main.pressure,
-        wind: data.wind,
+        wind_deg: data.wind.deg,
+        wind_speed: data.wind.speed,
         humidity: data.main.humidity,
         visibility: data.visibility,
         error: ""
@@ -36,7 +38,8 @@ class App extends Component {
         temperature: undefined,
         description: undefined,
         pressure: undefined,
-        wind: undefined,
+        wind_deg: undefined,
+        wind_speed: undefined,
         humidity: undefined,
         visibility: undefined,
         error: "Please enter valid values."
@@ -46,8 +49,20 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Weather App</h1>
+        <h1>Current weather</h1>
         <Form getWeather={this.getWeather} />
+        <WeatherInfo
+          city={this.state.city}
+          country={this.state.country}
+          temperature={this.state.temperature}
+          description={this.state.description}
+          pressure={this.state.pressure}
+          wind_deg={this.state.wind_deg}
+          wind_speed={this.state.wind_speed}
+          humidity={this.state.humidity}
+          visibility={this.state.visibility}
+          error={this.state.error}
+        />
       </div>
     );
   }
---
+++ src/components/WeatherInfo.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const WeatherInfo = (props) => {
+    const { city, country, temperature, description, pressure, wind_deg, wind_speed, humidity, visibility, error } = props;
+    const DisplayData =
+        (city && country) ?
+            (
+                <div>
+                    <h2>{city}, {country}</h2>
+                    <h1>{temperature} °</h1>
+                    <h2>{description}</h2>
+                    <p>{pressure} hPa</p>
+                    <p>{wind_deg} °, {wind_speed}m/s</p>
+                    <p>Humidity: {humidity}%</p>
+                    <p>Visibility: {visibility / 1000} km</p>
+                </div>
+            ) : (
+                <p>{error}</p>
+            )
+    return (
+        <div>
+            {DisplayData}
+        </div>
+    )
+}
+
+export default WeatherInfo
