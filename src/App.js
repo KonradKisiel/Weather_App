e59aeb6a9b998dd6c98187c0b39e2f4ab513226e
KonradKisiel <konrad.kisiel.webmaster@gmail.com>
2019-01-15 21:25:16 +0100

    active custom weather icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Form from './components/Form';
+
 import WeatherInfo from './components/WeatherInfo';
 import DefaultImg from './assets/img/simon-wilkes-345755-unsplash.jpg';
 import ClearImg from './assets/img/ian-dooley-407846-unsplash.jpg';
@@ -9,6 +10,17 @@ import DrizzleImg from './assets/img/christian-wiediger-672094-unsplash.jpg';
 import MistImg from './assets/img/ricardo-gomez-angel-223577-unsplash.jpg';
 import StormImg from './assets/img/layne-lawson-101816-unsplash.jpg';
 import SnowImg from './assets/img/nathan-fertig-371188-unsplash.jpg';
+
+import ClearSkyD from './assets/icons/01d.svg'
+import ClearSkyN from './assets/icons/01n.svg'
+import FewCloudsD from './assets/icons/02d.svg'
+import FewCloudsN from './assets/icons/02n.svg'
+import ScatteredClouds from './assets/icons/03d.svg'
+import BrokenClouds from './assets/icons/04d.svg'
+import ShowerRain from './assets/icons/09d.svg'
+import Thunderstorm from './assets/icons/11d.svg'
+import Snow from './assets/icons/13d.svg'
+import Mist from './assets/icons/50d.svg'
 
 //register to OpenWeatherMap and paste API key here
 const API_KEY = "37ed176a584be416c6960e386d5d239c";
@@ -41,6 +53,32 @@ class App extends Component {
         return DefaultImg;
     }
   }
+  setIcon = (icon) => {
+    switch (icon) {
+      case "01d":
+        return ClearSkyD;
+      case "01n":
+        return ClearSkyN;
+      case "02d":
+        return FewCloudsD;
+      case "02n":
+        return FewCloudsN;
+      case "03d", "03n":
+        return ScatteredClouds;
+      case "04d", "04n":
+        return BrokenClouds;
+      case "09d", "09n":
+        return ShowerRain;
+      case "11d", "11n":
+        return Thunderstorm;
+      case "13d", "13n":
+        return Snow;
+      case "50d", "50n":
+        return Mist;
+      default:
+        return null;
+    }
+  }
   getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
@@ -60,6 +98,7 @@ class App extends Component {
         wind_deg: data.wind.deg,
         wind_speed: data.wind.speed,
         humidity: data.main.humidity,
+        icon: this.setIcon(data.weather[0].icon)
       }
       this.setState({ weather, weatherInfo, newLocation: false })
       console.log(this.state);
@@ -68,13 +107,12 @@ class App extends Component {
       this.setState({
         weather: null,
         weatherInfo: null,
-        error: "Please enter valid values."
+        error: "Please enter valid values"
       })
     }
   };
 
   newLocation = () => {
-    console.log("click")
     this.setState({
       weather: null,
       weatherInfo: null,
@@ -94,15 +132,7 @@ class App extends Component {
         <img id="background" src={this.setBackground(weather)} alt="background" />
         {(weather && weatherInfo && !newLocation) ? (
           <WeatherInfo
-            city={weatherInfo.city}
-            country={weatherInfo.country}
-            temperature={weatherInfo.temperature}
-            description={weatherInfo.description}
-            pressure={weatherInfo.pressure}
-            wind_deg={weatherInfo.wind_deg}
-            wind_speed={weatherInfo.wind_speed}
-            humidity={weatherInfo.humidity}
-            error={weatherInfo.error}
+            weatherInfo={weatherInfo}
             newLocation={this.newLocation}
           />
         )
--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 const WeatherInfo = (props) => {
-    const { city, temperature, description, pressure, wind_deg, wind_speed, humidity, newLocation } = props;
+    const { city, temperature, description, pressure, wind_deg, wind_speed, humidity, icon } = props.weatherInfo;
     return (
         <div className="container">
             <h1 className="header">{city}</h1>
-            <h1 id="tempBig">{Math.floor(temperature)}°</h1>
+            <h1 id="temperatureBig">{Math.floor(temperature)}°</h1>
             <h2 className="capitalize">{description}</h2>
+            <img src={icon} alt="weather icon" />
             <p>{pressure} hPa</p>
             <p>{wind_deg}°, {wind_speed} m/s</p>
             <p>Humidity: {humidity}%</p>
             <br></br>
             <br></br>
             <br></br>
-            <button onClick={newLocation}>Change location</button>
+            <button onClick={props.newLocation}>Change location</button>
         </div>
     )
 }
